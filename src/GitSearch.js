0c0abcf826377ec75b4073ae24df8dfbfd7ab9b6
gfsouza <gsouza@daitangroup.com>
2018-08-21 16:59:16 -0300

    Implementacao de repositorios

--- src/GitSearch.js
+++ src/GitSearch.js
@@ -1,62 +1,94 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 
 export default class GitSearch extends Component {
-       constructor(props){
-         super(props);
-          this.state = {
-           username: '',
-           userrepo: '',
-          };
-       }
-
-       getUser(username) {
-          return fetch(`https://api.github.com/users/${username}`)
-          .then(response => response.json())
-          .then(response => {
-            return response;
-           })
-      }
-
-       async handleSubmit(e) {
-           e.preventDefault();
-           let user = await this.getUser(this.refs.username.value);
-           this.setState({ avatar_url: user.avatar_url,
-           username: user.login,
-           followers: user.followers,
-           following: user.following,
-           url: user.url,
-       });
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      repos: null,
+    };
   }
 
-     render() {
-       let user;
-       if(this.state.username) {
-          user =
-          <div className="results">
-            <img src={this.state.avatar_url} alt="user avatar"/>
-            <p className="userInfo">Username: {this.state.username}</p>
-            <p className="followerInfo">{this.state.followers} Followers</p>
-            <p className="followingInfo">Following {this.state.following} users</p>
-          </div>
-      }
-
-           return (
-              <div className="GitSearch">
-                <header className="Search-header">
-                  <h1>Github<br></br>User Search </h1>
-                </header>
-                <form onSubmit={e => this.handleSubmit(e)}>
-                   <input ref='username' type='text' placeholder='Username' />
-                   <button submit='username'>Search</button>
-               </form>
-               <div className="Search-body">{user}</div>
-             </div>
-           );
-    }
+getUser(username) {
+    axios.get(`https://api.github.com/users/${username}`)
+    .then(response => {
+      this.setState({
+        user: response.data
+      });
+    })
+    .catch(error => {
+        console.log(error);
+    });
 }
 
+getUserRepos(username) {
+    axios.get(`https://api.github.com/users/${username}/repos`)
+    .then(response => {
+       this.setState({
+         repos: response.data
+     });
+   })
+   .catch(error => {
+        console.log(error);
+   });
+}
+
+handleSubmit(e) {
+    e.preventDefault()
+
+    const { value } = this.refs.username
+    let user = this.getUser(value);
+    let repos = this.getUserRepos(value);
+
+    this.setState({user, repos})
+  }
+
+  displayUserRepos(repos) {
+    return repos.map(repository => {
+      return <div key={repository.id} className="repoResults">
+        <p> {repository.name} </p>
+      </div>
+    })
+  }
+
+  displayUser(user) {
+    return (
+      <div className="results">
+        <p className="userInfo">{user.name}</p>
+        <img src={user.avatar_url} alt="user avatar"/>
+        <p className="followerInfo">Followers: {user.followers}</p>
+        <p className="followingInfo">Following: {user.following}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {user, repos} = this.state
+
+    return (
+      <div className="GitSearch container">
+        <div className="col-md-8">
+          <header className="Search-header">
+            <h1>Github<br></br>User Search</h1>
+          </header>
+
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <input ref='username' type='text' placeholder='Username'/>
+            <button submit='username'>Search</button>
+          </form>
+
+          <div className="Search-body">
+            {user && this.displayUser(user)}
+            {repos && this.displayUserRepos(repos)}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+}
 
 ReactDOM.render(<GitSearch />, document.getElementById('root'));
