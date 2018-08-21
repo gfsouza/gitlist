3d88b58d01d28c5f2a3941df044babe293910d2c
Henrique Uzeda <hruzeda@gmail.com>
2018-08-20 23:22:41 -0300

    alterações de onti

--- src/GitSearch.js
+++ src/GitSearch.js
@@ -1,62 +1,86 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import axios from 'axios';
 
 export default class GitSearch extends Component {
-       constructor(props){
-         super(props);
-          this.state = {
-           username: '',
-           userrepo: '',
-          };
-       }
-
-       getUser(username) {
-          return fetch(`https://api.github.com/users/${username}`)
-          .then(response => response.json())
-          .then(response => {
-            return response;
-           })
-      }
-
-       async handleSubmit(e) {
-           e.preventDefault();
-           let user = await this.getUser(this.refs.username.value);
-           this.setState({ avatar_url: user.avatar_url,
-           username: user.login,
-           followers: user.followers,
-           following: user.following,
-           url: user.url,
-       });
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      userlist: null,
+      username: null,
+    };
   }
 
-     render() {
-       let user;
-       if(this.state.username) {
-          user =
-          <div className="results">
-            <img src={this.state.avatar_url} alt="user avatar"/>
-            <p className="userInfo">Username: {this.state.username}</p>
-            <p className="followerInfo">{this.state.followers} Followers</p>
-            <p className="followingInfo">Following {this.state.following} users</p>
-          </div>
-      }
+  componentDidMount() {
+    axios.get('https://api.github.com/users')
+      .then(response => {
+        this.setState({
+          userlist: response.data
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-           return (
-              <div className="GitSearch">
-                <header className="Search-header">
-                  <h1>Github<br></br>User Search </h1>
-                </header>
-                <form onSubmit={e => this.handleSubmit(e)}>
-                   <input ref='username' type='text' placeholder='Username' />
-                   <button submit='username'>Search</button>
-               </form>
-               <div className="Search-body">{user}</div>
-             </div>
-           );
+  getUser(username) {
+    axios.get(`https://api.github.com/users/${username}`)
+      .then(response => {
+        this.setState({
+          user: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUser(this.refs.username.value);
+  }
+
+  displayUser() {
+    if (this.state.user) {
+      return <div className="results">
+        <img src={this.state.user.avatar_url} alt="user avatar" />
+        <p className="userInfo">Username: {this.state.user.username}</p>
+        <p className="followerInfo">{this.state.user.followers} Followers</p>
+        <p className="followingInfo">Following {this.state.user.following} users</p>
+      </div>
     }
-}
+  }
 
+  displayList() {
+    if (this.state.userlist) {
+      return this.state.userlist.map(this.renderUserListRow)
+    }
+  }
+
+  renderUserListRow(userListRow, i) {
+    return <div className="resultlist">
+      <img src={userListRow.avatar_url} alt="avatar" />
+    </div>
+  }
+
+  render() {
+    return (
+      <div className="GitSearch container">
+        <header className="Search-header">
+          <h1>Github<br></br>User Search </h1>
+        </header>
+
+        <div className="row">{this.displayList()}</div>
+
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input ref='username' type='text' placeholder='Username' />
+          <button submit='username'>Search</button>
+        </form>
+        <div className="Search-body">{this.displayUser()}</div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<GitSearch />, document.getElementById('root'));
